ee413db86fe9d2ea8a655e6c08924d1e6c50ec92
Jeff Steward <steward.jeff@gmail.com>
2015-03-09 09:22:28 -0400

    new icons, fixed activity graphs, code reorg,...

--- build-museum.php
+++ build-museum.php
@@ -1,5 +1,5 @@
 <?PHP 
-	$apiBaseURL = "http://api.harvardartmuseums.org/collection/gallery";
+	$apiBaseURL = "http://api.harvardartmuseums.org";
 	$apiKey = "105ef4e0-3c22-11e3-a9a9-797cafc83c06";
 	$outputFile = "museum.json";
 
@@ -8,28 +8,34 @@
 	//Start building the museum;
 	print "Building the Harvard Art Museums" . PHP_EOL;
 	$museum = array("name"=>"Harvard Art Museums");
-
+	
 	//Build floors 1-3
-	for ($i=1; $i <= 3; $i++) { 
+	for ($i=0; $i <= 5; $i++) { 
 
-		$url = $apiBaseURL . "?apikey=" . $apiKey . "&q=floor:". $i ."&size=50&s=gallerynumber";	
+		$url = $apiBaseURL . "/gallery?apikey=" . $apiKey . "&q=floor:". $i ."&size=50&sort=gallerynumber";	
 		$r = file_get_contents($url);
 		$m = json_decode($r,true);
 
 		print "Building floor " . $i . PHP_EOL;
 
-		$floor = array("name"=>"Floor " . $i, "children"=>array());
+		$floor = array("name"=>"Level " . $i, "children"=>array());
 		
 		foreach ($m["records"] as $record) {
-			print "Building gallery " . $record["gallerynumber"] . PHP_EOL;
+			print "Building gallery " . $record["id"] . PHP_EOL;
 			
-			$url = $apiBaseURL . "/" . $record["gallerynumber"] . "?apikey=" . $apiKey;
+			$url = $apiBaseURL . "/gallery/" . $record["id"] . "?apikey=" . $apiKey;
 			$r = file_get_contents($url);
-
 			$gallery = json_decode($r, true);
-			
-			$gallery["children"] = $gallery["objects"];
-			unset($gallery["objects"]);
+			unset($gallery["contains"]);
+			unset($gallery["lastupdate"]);
+			unset($gallery["id"]);
+
+			$url = $apiBaseURL . "/object?apikey=" . $apiKey . "&size=200&fields=objectnumber,title,primaryimageurl,classification,dated,url&gallery=" . $record["gallerynumber"];			
+			$r = file_get_contents($url);
+			$objects = json_decode($r, true);
+
+			$gallery["objectcount"] = count($objects["records"]);
+			$gallery["children"] = $objects["records"];
 			
 			$floor["children"][] = $gallery;
 		}
